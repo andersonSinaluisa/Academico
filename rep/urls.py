539418933bd1868933f0afcec62b308b85eabde9
eliot <eisuarez@est.itsgg.edu.ec>
2021-05-07 11:55:59 -0500

    Subo cambios en caso de perder lo avanzado

--- rep/urls.py
+++ rep/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from reg.views import *
+from rep.views import *
 
 urlpatterns = [
-
+    path("reporte-cursos/", ReporteCurso.as_view(), name="reporte_cursos"),
+    path("reporte-profesores/", ReporteProfesor.as_view(), name="reporte_profesores"),
+    path("reporte-matricula/", ReporteMatricula.as_view(), name="reporte_matricula"),
+    # url de metodos para consultar cursos y secciones en matricula y estudiantes
+    path("consultar-cursos/", con_jor, name="consultar_cursos"),
+    path("consultar-secciones/", con_cur, name="consultar_secciones"),
 ]
